a02daaeab62cbfcfadf8422fa243ce9def99b678
irtixa <syed.15634@iqra.edu.pk>
2021-12-13 18:13:35 +0500

    Done with description section.

--- src/App.js
+++ src/App.js
@@ -1,91 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { Modal, Button } from 'antd';
-import React, { useState } from 'react';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./screens/Home"
+import About from "./screens/About"
+import {obj as o} from "./screens/Home"
 
-import { Form, Input } from 'antd';
+
+
 function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [issModalVisible, setissModalVisible] = useState(false);
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-    setissModalVisible(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-  const showModal2 = () => {
-
-    setissModalVisible(true);
-  };
-
-
-
-
+  console.log(o);
   return (
-    <>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-
-      <Modal class="M2" title="Add " bodyStyle={{ height: 200 }}  width={1000}  visible={issModalVisible}>
-
-        <div>
-          <div class="L">
-            <Button type="primary" block>
-              Primary
-            </Button>
-            <Button block>Default</Button>
-            <Button type="dashed" block>
-              Dashed
-            </Button>
-            <Button type="link" block>
-              Link
-            </Button>
-          </div>
-          <div class="R">rr</div>
-        </div>
-
-
-      </Modal>
-
-      <Modal title="+ Add Description" visible={isModalVisible} onOk={handleOk} okText="Next" onClick={showModal2} onCancel={handleCancel}>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-
-        >
-          <Form.Item
-            label="Automation name *"
-            name="Autoname"
-
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Project name *"
-            name="Pname"
-
-          >
-            <Input />
-          </Form.Item>
-        </Form>
-
-      </Modal>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        {/* <About /> */}
+        {/* </Route> */}
+      </Switch>
+    </Router>
   );
-
 }
 
 export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,182 @@
+import "../App.css";
+import { Modal, Button } from "antd";
+import React, { useState } from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { Tabs, Radio, Space, Form, Input } from "antd";
+import {
+  PlusCircleFilled,
+  CalendarOutlined,
+  BorderBottomOutlined,
+  BgColorsOutlined,
+} from "@ant-design/icons";
+import { Card, Avatar } from "antd";
+const { TabPane } = Tabs;
+const { Meta } = Card;
+
+function Home() {
+  const [tabPosition, setTabPosition] = useState("left");
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [issModalVisible, setissModalVisible] = useState(false);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const show = () => {
+    setissModalVisible(false);
+    setIsModalVisible(false);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    setissModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setissModalVisible(false);
+  };
+  const showModal2 = () => {
+    setissModalVisible(true);
+  };
+
+  return (
+    <>
+      <div classname="OM">
+        <Button
+          style={{
+            width: 170,
+            height: 100,
+            fontSize: 25,
+            marginTop: 350,
+            marginLeft: 800,
+          }}
+          type="primary"
+          onClick={showModal}
+        >
+          Open Modal
+        </Button>
+      </div>
+      <Modal
+        class="M2"
+        title="When will your attomation start ? "
+        bodyStyle={{ height: 350 }}
+        width={1000}
+        okButtonProps={{ style: { display: "none" } }}
+        visible={issModalVisible}
+        onCancel={handleCancel}
+      >
+        <div>
+          <Tabs tabPosition={tabPosition} size="large">
+            <TabPane tab="Lead Trigger" key="1">
+              <Card
+                style={{
+                  width: 650,
+                  marginTop: 0,
+                  height: 80,
+                  borderBottomColor: "#4de06f",
+                  borderBottomWidth: 4,
+                }}
+                onClick={show}
+              >
+                <Meta
+                  avatar={
+                    <PlusCircleFilled
+                      style={{ color: "#4de06f", fontSize: "large" }}
+                    />
+                  }
+                  title="New Lead"
+                  description="when   a    New   Lead   is   created   or    added"
+                />
+              </Card>
+
+              <Card
+                style={{
+                  width: 650,
+                  marginTop: 25,
+                  height: 80,
+                  borderBottomColor: "#4de06f",
+                  borderBottomWidth: 4,
+                }}
+              >
+                <Meta
+                  avatar={
+                    <PlusCircleFilled
+                      style={{ color: "#4de06f", fontSize: "large" }}
+                    />
+                  }
+                  title="Lead Update"
+                  description="When a Lead field is updated or changedd"
+                />
+              </Card>
+
+              <Card
+                style={{
+                  width: 650,
+                  marginTop: 25,
+                  height: 80,
+                  borderBottomColor: "#4de06f",
+                  borderBottomWidth: 4,
+                }}
+              >
+                <Meta
+                  avatar={
+                    <CalendarOutlined
+                      style={{ color: "#4de06f", fontSize: "large" }}
+                    />
+                  }
+                  title=" On a Specific Date"
+                  description="Such as Birthday, Renewal ,etc"
+                />
+              </Card>
+            </TabPane>
+            <TabPane
+              tab=" Opportunity Trigger "
+              key="2"
+              style={{ borderBottomColor: "#8fa5ba", borderBottomWidth: 4 }}
+            >
+              “Content for the selected tab will be updated later”
+            </TabPane>
+            <TabPane tab=" Activity Trigger " key="3">
+              “Content for the selected tab will be updated later”
+            </TabPane>
+            <TabPane tab="User Trigger" key="4">
+              “Content for the selected tab will be updated later”
+            </TabPane>
+            <TabPane tab="Task Trigger" key="5">
+              “Content for the selected tab will be updated later”
+            </TabPane>
+          </Tabs>
+        </div>
+      </Modal>
+
+      <Modal
+        title="+ Add Description"
+        visible={isModalVisible}
+        onOk={handleOk}
+        okText="Next"
+        onClick={showModal2}
+        onCancel={handleCancel}
+      >
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+        >
+          <Form.Item label="Automation name *" name="Autoname">
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Project name *" name="Pname">
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+}
+
+export default Home;
+
+export const obj = { firstName: "ali", lastName: "haider" };
+
